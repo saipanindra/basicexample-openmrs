b5b33440a5cd49192cbd9e5a681b61fbe9c0b78f
saipanindra9@gmail.com <saipanindra9@gmail.com>
2015-06-13 22:31:42 -0700

    Add name and description fields

--- api/src/main/java/org/openmrs/module/basicexample/mymodule.java
+++ api/src/main/java/org/openmrs/module/basicexample/mymodule.java
@@ -25,7 +25,26 @@ public class mymodule extends BaseOpenmrsObject implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	private String name;
+	private String description;
 	
+	
+	public String getName() {
+		return name;
+	}
+
+		public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	@Override
 	public Integer getId() {
 		return id;
